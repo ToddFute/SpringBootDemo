fa094350c49b8fa18c5ae848d2b3d696e479ee38
ToddBfute <tbradfute@workforcesoftware.com>
2017-01-25 07:53:54 -0500

    Show the transformation of the project.

---
+++ showit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+git checkout save1
+gvim 1_2.txt
+read -n1 -r -p "Press space to continue..." key
+git checkout save2
+gvim 2_3.txt
+read -n1 -r -p "Press space to continue..." key
+git checkout save3
+gvim 3_4.txt
+read -n1 -r -p "Press space to continue..." key
+git checkout save4
+gvim 4_5.txt
+read -n1 -r -p "Press space to continue..." key
+git checkout save5
+gvim 5_6.txt
+read -n1 -r -p "Press space to continue..." key
+git checkout save6
+gvim 6_7.txt
+read -n1 -r -p "Press space to continue..." key
+git checkout save7
+gvim 7_8.txt
+read -n1 -r -p "Press space to continue..." key
+git checkout save8
